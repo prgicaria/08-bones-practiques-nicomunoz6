cdd731aa29d08b400a54dfab96ed02a38cb35278
nicomunoz6 <nmunoz@instituticaria.cat>
2025-10-27 08:11:26 +0100

    Enviament

--- .github/bones_practiques.py
+++ .github/bones_practiques.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+'''Nom del programa. Descripció curta del programa
+Institut Icària
+Programació - 1r Batxillerat - Curs 2023-24
+Descripció llarga del programa. Indicar que fa el
+programa i el resultat esperat.
+'''
+__author__ = "One solo developer"
+__authors__ = ["One developer", "And another one", "etc"]
+__email__ = "mail@example.com"
+__date__ = "YYYY/MM/DD"
 Dividend = int(input("ingresar dividend"))
 Divisor = int(input("ingresar divisor"))
 Quocient = (Dividend)//(Divisor)
